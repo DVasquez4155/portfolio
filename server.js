80dbcb6419b651010e09dff2761485a4e6a0fa86
Daniel V <main@dvasquez4155.com>
2020-04-25 12:39:59 -0700

    Added Express

---
+++ server.js
@@ -0,0 +1,31 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Requiring necessary npm packages
+var express = require("express");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("app/public"));
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+// Routes
+require("./app/routes/api.js")(app);
+require("./app/routes/html.js")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
