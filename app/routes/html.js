d3fd08f5a5efe88cb1a673a7d79c0bb3139dece1
Daniel V <main@dvasquez4155.com>
2020-04-27 14:35:41 -0700

    Prank url

--- app/routes/html.js
+++ app/routes/html.js
@@ -1,4 +1,8 @@
 module.exports = function (app) {
+    //Pranking Link
+    app.get("/video/:video", (req,res) => {
+        res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    })
     app.get("/", function (req, res) {
         res.sendFile('views/index.html', {
             root: process.cwd()
