087e558101261428222a349833cefa4dd74fcdd1
Daniel V <main@dvasquez4155.com>
2020-05-14 18:51:20 -0700

    Updated to react

---
+++ precache-manifest.a1008eec7b99872bfd9779e90ea0691b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777779fa8c847c5636dbef62b248ec79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "974481615c3eb0370771",
+    "url": "/static/css/main.091f6fb6.chunk.css"
+  },
+  {
+    "revision": "ae8d480b4dc078b2d07f",
+    "url": "/static/js/2.dce88638.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dce88638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974481615c3eb0370771",
+    "url": "/static/js/main.92d4a3ce.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "867cf8f12853eb35a5c2f196d9da0864",
+    "url": "/static/media/avatar.867cf8f1.png"
+  }
+]);
